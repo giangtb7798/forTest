c2ebf55b8b19fd4c72c916be0f8e8c08fd8f1faa
giangsota <hieu.nguyen@setel.com>
2022-12-05 15:42:31 +0700

    new script

--- MyFirstBranch.java
+++ MyFirstBranch.java
@@ -1,11 +1,3 @@
 import java.io.*;
 public class MyFirstBranch {
-    //i just have some changes here
-    //this is my test
-    //changes on main branch
-
-
-    //main has changed
-    //haha continues test
-    //changes on main branch
 }
